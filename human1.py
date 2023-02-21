a4ab9b99d79404f367cff64be4a70753d2d2dbc2
homeoffice-cyber <yochanan.steinberg@gmail.com>
2023-02-21 13:19:04 -0500

    first commit

---
+++ human1.py
@@ -0,0 +1,31 @@
+#this is yocheved's first
+
+a = 1
+b = 2
+
+print('a+b=', a+b)
+
+name = 'Eric Cartman'
+
+print('the name is ', name)
+naem = 'Butt Face'
+print(naem)
+'''this thing is 
+stupid be a 
+loser'''
+print(name)
+site_name = 'gargoyel'
+
+print(site_name)
+# declare constants
+PI = 3.14
+GRAVITY = 9.8
+
+
+some_humans = 'SH'
+print(some_humans)
+
+humans_of_the_planet = 'you are a loser'
+print(humans_of_the_planet)
+
+print(some_humans + ' ' + humans_of_the_planet)
