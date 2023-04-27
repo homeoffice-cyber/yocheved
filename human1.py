5c2c15227ef633c768b753af6677c39fec59b1d6
homeoffice-cyber <yochanan.steinberg@gmail.com>
2023-04-27 19:13:25 -0400

    Functon and strings variables

--- human1.py
+++ human1.py
@@ -22,10 +22,60 @@ PI = 3.14
 GRAVITY = 9.8
 
 
-some_humans = 'SH'
-print(some_humans)
+some_humans = 'SHHHHHH'
+print('this is what the string variable some_humnans contains: ', some_humans)
 
-humans_of_the_planet = 'you are a loser'
+humans_of_the_planet = 'you are_a_loser'
 print(humans_of_the_planet)
 
 print(some_humans + ' ' + humans_of_the_planet)
+x = 4
+x = "Sally"
+print(x)
+x = str(3)
+y = int(3)
+z = float(3)
+print(x+'a')
+print(type(y))
+print(type(z))
+x = 'loser_head'
+print(x)
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+vegies = 'tomstoed'
+x = y= z = vegies
+print(x)
+print(y)
+print(z)
+x ='trash'
+y = 'idiocy'
+z = 'me_man'
+print(x + y + z)
+x = 5
+z = 10
+print(z + x)
+x = 25
+y = 89
+print(y + x)
+x = 45-87
+y = 89-98
+print(x + y)
+# x = "awesome"
+
+
+def myfunc1(x):
+  print("Python is " + x)
+
+def myfunc2(z):
+    print("you are a " + z)
+
+
+myfunc1("awesome")
+
+myfunc2("idiot")
